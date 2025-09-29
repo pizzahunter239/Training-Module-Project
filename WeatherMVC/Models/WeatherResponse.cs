80b97e179c5baa1b768e3f385f279120fcc4681a
pizzahunter239 <pizzahunter239@gmail.com>
2025-09-29 17:00:38 -0400

    Master version of project

---
+++ WeatherMVC/Models/WeatherResponse.cs
@@ -0,0 +1,33 @@
+﻿//STUDENTS WILL MAKE THIS FILE
+
+namespace WeatherMVC.Models
+{
+    // Root response object
+    public class WeatherResponse
+    {
+        public Location Location { get; set; }
+        public Current Current { get; set; }
+    }
+
+    public class Location
+    {
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+    }
+
+    public class Current
+    {
+        public double Temp_c { get; set; }
+        public double Temp_f { get; set; }
+        public Condition Condition { get; set; }
+        public double Humidity { get; set; }
+        public double Feelslike_f { get; set; }
+    }
+
+    public class Condition
+    {
+        public string Text { get; set; }
+        public string Icon { get; set; }
+    }
+}
---
+++ WeatherMVC/Program.cs
@@ -0,0 +1,43 @@
+namespace WeatherMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            // *** REMOVE FOR STUDENT VERSION - START ***
+            // Register HttpClient for dependency injection
+            // INSTRUCTION NOTE: Explain that this allows us to inject HttpClient
+            // into our controllers to make API calls
+            builder.Services.AddHttpClient();
+            // *** REMOVE FOR STUDENT VERSION - END ***
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
